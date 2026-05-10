6e9bb8bf3f25ef3f6db972124d744f3c86071828
Tom Cuel <tom.cuel3@gmail.com>
2026-05-11 01:53:46 +0200

    NRT DCGans (add the compil results)

--- NRT/NRT_GANs/test.py
+++ NRT/NRT_GANs/test.py
@@ -759,6 +759,162 @@ def test_cgan_gan_mnist():
     plt.close()
 
 
+def test_dcgan_mnist():
+    """
+    Test DCGAN on MNIST dataset with default loss function
+    
+    This function initializes a DCGAN with default configuration,
+    and tests its forward pass and loss computation,
+    and then verify the sampling (with ema)
+    """
+    print("Loading MNIST dataset...")
+    mnist_loader = load_mnist(batch_size=128, train=True)  
+
+    # Plot real data distribution
+    # first_numbers = next(iter(mnist_loader))[0][:5]  # Get the first 5 samples of the first batch
+    # print(first_numbers.shape)  # torch.Size([5, 784]) for 28x28 downsampled images
+    # plot_images(first_numbers, 5, save_path=os.path.join(MNIST_OUTPUT_DIR,"mnist_real.png"), title="Image 1 from MNIST dataset")
+
+    # Define GAN configuration
+    config = GANConfig(
+        architecture="DCGAN",
+        loss="Default",
+        latent_dim=64,
+        image_size=28,
+        image_channels=1,
+        hidden_dims=[128, 64],
+        kernel_size=4,
+        stride=2,
+        padding=1,
+        dropout=0.0,
+        batch_norm=True,
+        spectral_norm_on=False, # too slow otherswise for this test
+        learning_rate=2e-4,
+        step_size=20,
+        weight_decay=0.0,
+        beta1=0.5,
+        beta2=0.999,    
+        is_ema=False,
+        ema_decay=0.999
+    )
+    print("GAN configuration:")
+    print(config)
+    print()
+
+    # Initialize GAN
+    device = "cpu"
+    gan = GAN(config, device=device)
+    print("GAN Architecture:")
+    print(gan)
+    print()
+
+    # Train the model for a few epochs then plot the generated samples, then repeat the process
+    epochs = 10
+    nb_reps = 5
+    TEST_OUTPUT_DIR = os.path.join(MNIST_OUTPUT_DIR, "dcgan_mnist")
+    os.makedirs(TEST_OUTPUT_DIR, exist_ok=True)
+    G_losses = []
+    D_losses = []
+    for rep in range(nb_reps):
+        print(f"Training DCGAN on MNIST dataset - Rep {rep+1}/{nb_reps} - Epochs: {epochs*rep} to {epochs*(rep+1)}")
+        history = gan.fit(mnist_loader, epochs=epochs, verbose=False)
+        G_losses.extend([hist.G_loss for hist in history])
+        D_losses.extend([hist.D_loss for hist in history])
+        samples = gan.sample(5).numpy()
+        plot_images(samples, 5, save_path=os.path.join(TEST_OUTPUT_DIR,f"mnist_gen_rep{rep+1}.png"), title=f"Generated Images from DCGAN - Rep {rep+1}")
+        print()
+
+    # Plot loss history
+    plt.figure()    
+    plt.plot(G_losses, label="Generator Loss", color='red')
+    plt.plot(D_losses, label="Discriminator Loss", color='blue')
+    plt.legend()
+    plt.title("DCGAN Loss History on MNIST dataset")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.savefig(os.path.join(TEST_OUTPUT_DIR,"loss_history.png"))
+    plt.close()
+
+
+def test_dcgan_wasserstein_mnist():
+    """
+    Test DCGAN on MNIST dataset with wasserstein loss function and gradient penalty
+
+    This function initializes a DCGAN with wasserstein loss and gradient penalty configuration,
+    and tests its forward pass and loss computation,
+    and then verify the sampling (without ema)
+    """
+    print("Loading MNIST dataset...")
+    mnist_loader = load_mnist(batch_size=128, train=True)
+
+    # Plot real data distribution
+    # first_numbers = next(iter(mnist_loader))[0][:5]  # Get the first 5 samples of the first batch
+    # print(first_numbers.shape)  # torch.Size([5, 784]) for 28x28 downsampled images
+    # plot_images(first_numbers, 5, save_path=os.path.join(MNIST_OUTPUT_DIR,"mnist_real.png"), title="Image 1 from MNIST dataset")
+
+    # Define GAN configuration
+    config = GANConfig(
+        architecture="DCGAN",
+        loss="Wasserstein",
+        latent_dim=64,
+        image_size=28,
+        image_channels=1,
+        hidden_dims=[128, 64],
+        kernel_size=4,
+        stride=2,
+        padding=1,
+        weight_clip=0.01,
+        gradient_penalty_lambda=10.0,
+        n_critic=5,
+        dropout=0.0,
+        batch_norm=True,
+        spectral_norm_on=False, # too slow otherswise for this test
+        learning_rate=1e-4,
+        step_size=20,
+        weight_decay=0.0,
+        beta1=0.0,
+        beta2=0.9,
+        is_ema=False,
+        ema_decay=0.999
+    )
+    print("GAN configuration:")
+    print(config)
+    print()
+
+    # Initialize GAN
+    device = "cpu"
+    gan = GAN(config, device=device)
+    print("GAN Architecture:")
+    print(gan)
+
+    # Train the model for a few epochs then plot the generated samples, then repeat the process
+    epochs = 10
+    nb_reps = 5
+    TEST_OUTPUT_DIR = os.path.join(MNIST_OUTPUT_DIR, "dcgan_wgan_mnist")
+    os.makedirs(TEST_OUTPUT_DIR, exist_ok=True)
+    G_losses = []
+    D_losses = []
+    for rep in range(nb_reps):
+        print(f"Training DCGAN with Wasserstein loss and gradient penalty on MNIST dataset - Rep {rep+1}/{nb_reps} - Epochs: {epochs*rep} to {epochs*(rep+1)}")
+        history = gan.fit(mnist_loader, epochs=epochs, verbose=False)
+        G_losses.extend([hist.G_loss for hist in history])
+        D_losses.extend([hist.D_loss for hist in history])
+        samples = gan.sample(5).numpy()
+        plot_images(samples, 5, save_path=os.path.join(TEST_OUTPUT_DIR,f"mnist_gen_rep{rep+1}.png"), title=f"Generated Images from DCGAN Wasserstein GAN - Rep {rep+1}")
+        print()
+
+    # Plot loss history
+    plt.figure()
+    plt.plot(G_losses, label="Generator Loss", color='red')
+    plt.plot(D_losses, label="Discriminator Loss", color='blue')
+    plt.legend()
+    plt.title("DCGAN with Wasserstein loss and gradient penalty Loss History on MNIST dataset")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.savefig(os.path.join(TEST_OUTPUT_DIR,"loss_history.png"))
+    plt.close()
+
+
 if __name__ == "__main__":
     print_section("Testing Generator and Discriminator architectures")
 
@@ -783,6 +939,10 @@ if __name__ == "__main__":
 
     print_subsection("Testing CGAN on MNIST dataset")
     # test_cgan_gan_mnist()
+
+    print_subsection("Testing DCGAN on MNIST dataset")
+    test_dcgan_mnist()
+    test_dcgan_wasserstein_mnist()
     
     print("All tests completed successfully!")
     # clear_data_dir()
